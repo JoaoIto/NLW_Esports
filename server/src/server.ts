095e21b198055d5138b1671029ad3fe65b0f97f0
João <joaovictorpfr@gmail.com>
2023-06-26 09:55:02 -0300

    component game

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,19 +1,68 @@
-// Importando dependências;
-
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
-https://localhost:3333/ads
 
-app.get("/ads", (request, response) => {
-    console.log("Acessou ads");
-    return response.json([
-        {id: 1, name: "Anuncio 1"},
-        {id: 2, name: "Anuncio 2"},
-        {id: 3, name: "Anuncio 3"},
-        {id: 4, name: "Anuncio 4"},
-        {id: 5, name: "Anuncio 5"},
-    ]);
+app.get("/", async (requeste, response) => {
+  return response.status(200).json("Start server!");
+})
+/* https://localhost:3333/ */
+const prisma = new PrismaClient({
+  log: ["query"],
+});
+// Listagem de games:
+
+app.get("/games", async (requeste, response) => {
+  const games = await prisma.game.findMany({
+    include: {
+      _count: {
+        select: {
+          ads: true,
+        },
+      },
+    },
+  });
+  return response.json([games]);
+});
+
+// Criação de um anúncio:
+/* Usado método status(),  
+indicando qual o formato que foi aceito: 
+de sucesso ou não, de resposta da API é valida, etc... */
+
+app.post("/ads", (requeste, response) => {
+  return response.status(201).json([]);
+});
+
+// listagem de anúncios por game...
+/* Colocada a propriedade dinâmica de ":" dois pontos, 
+para que consigamos listar o ID do anúncios de game 
+em sua resposta JSON */
+
+app.get("/games/:id/ads", async (request, response) => {
+  const gameID = request.params.id;
+  const ads = await prisma.ad.findMany({
+    select: {
+        id: true,
+        name: true, 
+        weekDays: true,
+        useVoiceChannel: true,
+        yearsPlaying: true,
+        hoursStart: true,
+    },
+
+    orderBy:{
+        createAt: 'desc'
+    }
+  });
+  return response.json([ads]);
+});
+
+// Buscar discord do player com ads ID
+
+app.get("/ads/:id/discord", (request, response) => {
+  // const adID =  request.params.id;
+  return response.json([]);
 });
 
 app.listen(3333);
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,4 @@
+import { GameBanner } from "./components/GameBanner";
 import "./styles/main.css";
 import { MagnifyingGlassPlus } from "phosphor-react";
 function App() {
@@ -13,52 +14,12 @@ function App() {
       </h1>
 
       <div className="grid grid-cols-6 gap-6 mt-16">
-        <a href="" className="relative rounded-lg overflow-hidden">
-          <img src="/game1.png" alt="" />
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-            <strong className="font-bold text-white block">
-              League of Legends
-            </strong>
-            <span className="text-zinc-300 text-sm block">4 anúncios</span>
-          </div>
-        </a>
-        <a href="" className="relative rounded-lg overflow-hidden">
-          <img src="/game2.png" alt="" />
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-            <strong className="font-bold text-white block">Dota 2 </strong>
-            <span className="text-zinc-300 text-sm block">4 anúncios</span>
-          </div>
-        </a>
-        <a href="" className="relative rounded-lg overflow-hidden">
-          <img src="/game3.png" alt="" />
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-            <strong className="font-bold text-white block">CS GO</strong>
-            <span className="text-zinc-300 text-sm block">4 anúncios</span>
-          </div>
-        </a>
-        <a href="" className="relative rounded-lg overflow-hidden">
-          <img src="/game4.png" alt="" />
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-            <strong className="font-bold text-white block">Apex Legends</strong>
-            <span className="text-zinc-300 text-sm block">4 anúncios</span>
-          </div>
-        </a>
-        <a href="" className="relative rounded-lg overflow-hidden">
-          <img src="/game5.png" alt="" />
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-            <strong className="font-bold text-white block">Fortnite</strong>
-            <span className="text-zinc-300 text-sm block">4 anúncios</span>
-          </div>
-        </a>
-        <a href="" className="relative rounded-lg overflow-hidden">
-          <img src="/game6.png" alt="" />
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-            <strong className="font-bold text-white block">
-              World of WarCraft
-            </strong>
-            <span className="text-zinc-300 text-sm block">4 anúncios</span>
-          </div>
-        </a>
+        <GameBanner bannerUrl="/public/game1.png" title="League of Legends" addCount={5}/>
+        <GameBanner bannerUrl="/public/game2.png" title="Dota 2" addCount={2}/>
+        <GameBanner bannerUrl="/public/game3.png" title="CS GO" addCount={12}/>
+        <GameBanner bannerUrl="/public/game4.png" title="Apex Legends" addCount={11}/>
+        <GameBanner bannerUrl="/public/game5.png" title="Fortnite" addCount={4}/>
+        <GameBanner bannerUrl="/public/game6.png" title="World Warwcraft" addCount={5}/>
       </div>
       <div className="pt-1 bg-nlwGradientText mt-8 self-stretch rounded-lg overflow-hidden">
         <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
